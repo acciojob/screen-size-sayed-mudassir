21a1e972909331b3fe25ab85228e82d86380f8a3
sayed mudassir <sayedmuddassir68@gmail.com>
2024-05-30 02:23:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const size = document.getElementById("size");
+
+function updateScreenSzie(e){
+    size.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+}
+
+window.addEventListener("resize",(e)=>{
+    size.innerText = `Width: ${e.target.innerWidth} and Height: ${e.target.innerHeight}`;
+});
+updateScreenSzie();
